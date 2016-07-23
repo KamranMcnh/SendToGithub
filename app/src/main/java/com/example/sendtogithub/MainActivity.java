bb4771b3eec124420cc1bf652164722b3d9e8fc7
KamranMcnh <cameldashcase@gmail.com>
2016-07-23 10:27:41 -0700

    first iniit

--- app/src/main/java/com/example/sendtogithub/MainActivity.java
+++ app/src/main/java/com/example/sendtogithub/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // this comment is to confirm that it works
+        //this comment confirms the data update
     }
 }
